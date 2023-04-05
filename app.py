48eee2dbd7caf6e8fa1d99c22d0fc6ba4af02d8c
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-04-05 20:29:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 menu = """
 <a href="/">Página inicial</a> | <a href="/noticias">NOTÍCIAS INDÍGENAS</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
-<br>
+<br> | <a href="/planilha">Sobre</a>
 """
 
 @app.route("/")
@@ -51,75 +51,8 @@ def noticias_indigenas():
   df=pd.DataFrame(lista_noticias, columns=['Manchete','Link'])
   return df
 
-
-# Autenticando a API do Google Sheets
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/credentials.json', scope)
-client = gspread.authorize(creds)
-
-# Abrindo a planilha existente
-planilha = client.open('robo_lucasduarte_bot').sheet1
-
-# Adicionando novas linhas à planilha
-novas_linhas = [['Manchete 1', 'Link 1'], ['Manchete 2', 'Link 2']]
-planilha.add_rows(len(novas_linhas))
-for i, linha in enumerate(novas_linhas):
-    planilha.update(f'A{i+2}:B{i+2}', [linha])
-
-
-
-  def processa_mensagens():
-  # Seria interessante tratarmos duas possíveis exceções:
-  # 1- caso a sheet não exista, criá-la
-  # 2- caso a célula não esteja preenchida, preenchê-la com 0
-
-  # Pega na planilha do sheets o último update_id
-  update_id = int(sheet.get("A1")[0][0])
-  # Parâmetros de uma URL - também são chamados de query strings
-  resposta = requests.get(f"https://api.telegram.org/bot{token}/getUpdates?offset={update_id + 1}")
-  dados = resposta.json()["result"]  # lista de dicionários (cada dict é um "update")
-  print(f"Temos {len(dados)} novas atualizações:")
-  mensagens = []
-  for update in dados:
-    update_id = update["update_id"]
-    if "message" not in update:
-      print(f"ERROR: not a menssagem: {update}")
-      continue
-    # Extrai dados para mostrar mensagem recebida
-    first_name = update["message"]["from"]["first_name"]
-    sender_id = update["message"]["from"]["id"]
-    if "text" not in update["message"]:
-      continue  # Essa mensagem não é um texto!
-    message = update["message"]["text"]
-    chat_id = update["message"]["chat"]["id"]
-    datahora = str(datetime.datetime.fromtimestamp(update["message"]["date"]))
-    if "username" in update["message"]["from"]:
-      username = update["message"]["from"]["username"]
-    else:
-      username = "[não definido]"
-    print(f"[{datahora}] Nova mensagem de {first_name} @{username} ({chat_id}): {message}")
-    mensagens.append([datahora, "recebida", username, first_name, chat_id, message])
-    # Define qual será a resposta e envia
-    if message == "/start":
-      texto_resposta = "Olá! Seja bem-vinda(o). *Digite* /noticias para ver as últimas."
-
-    elif message.lower().strip() in ["/noticias", "/notícias"]:  # Tratamento da mensagem
-      noticias = captura_ultimas_noticias()
-      texto_resposta = "Encontrei as seguintes notícias sobre indígenas:\n"
-      for noticia in noticias:
-        texto_resposta += f"- {noticia['title']} ({noticia['url']})\n"
-
-    else:
-      texto_resposta = "Não entendi!"
-    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-    requests.post(f"https://api.telegram.org./bot{token}/sendMessage", data=nova_mensagem)
-    mensagens.append([datahora, "enviada", username, first_name, chat_id, texto_resposta])
-  # Atualiza planilha do sheets com último update processado
-  sheet.append_rows(mensagens)
-  sheet.update("A1", update_id)
-  
-  import time
-
-while True:
-  processa_mensagens()
-  time.sleep(5)
+@app.route("/planilha")
+def planilha():
+  lista = df.values.tolist()
+  sheet.append_rows(lista)
+  return "Planilha escrita!"
